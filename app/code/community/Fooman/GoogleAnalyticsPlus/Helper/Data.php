51c4c5ef3facb85300aedb040d78ed6f5968f70f
Robert Egginton <robert.egginton@c3media.co.uk>
2015-08-05 15:30:09 +0100

    Replaced ecommerce calls with enhanced ec calls in universal and ajax. Added additional calls in ajax to set options from shipping and payment

--- app/code/community/Fooman/GoogleAnalyticsPlus/Helper/Data.php
+++ app/code/community/Fooman/GoogleAnalyticsPlus/Helper/Data.php
@@ -79,4 +79,87 @@ class Fooman_GoogleAnalyticsPlus_Helper_Data extends Mage_Core_Helper_Abstract
         }
     }
 
+    /**
+     * @param Mage_Sales_Model_Order|Mage_Sales_Model_Quote $order
+     *
+     * @return string
+     */
+    public function getCartProductEC($order)
+    {
+        $return = '';
+        $altUniversal = Mage::getStoreConfig('google/analyticsplus_universal/altaccountnumber');
+
+        foreach ($order->getAllVisibleItems() as $item) {
+            $itemDetails = "{
+                'id': '" . $this->jsQuoteEscape($item->getSku()) . "',
+                'name': '" . $this->jsQuoteEscape($item->getName()) . "',
+                'sku': '" . $this->jsQuoteEscape($item->getSku()) . "',
+                'price': '"
+                . Mage::helper('googleanalyticsplus')->convert($item, 'price', ($order instanceof Mage_Sales_Model_Order) ? $order->getOrderCurrencyCode() : $order->getQuoteCurrencyCode())
+                . "',
+                'quantity': '" . (int)$item->getQtyOrdered() . "',
+                'category': '" . $this->jsQuoteEscape($this->getCategory($item))
+                . "'
+            }";
+
+            $return .= "ga('ec:addProduct',{$itemDetails});\n";
+            if ($altUniversal) {
+                $return .= "ga('" . Fooman_GoogleAnalyticsPlus_Block_Universal::TRACKER_TWO_NAME . ".ec:addProduct',{$itemDetails});\n";
+            }
+        }
+
+        return $return;
+    }
+
+    /**
+     * retrieve an item's category
+     * if no product attribute chosen use the product's first category
+     *
+     * @param $item
+     *
+     * @return mixed|null
+     */
+    protected function getCategory($item)
+    {
+
+        $product = Mage::getModel('catalog/product')->load($item->getProductId());
+        if ($product) {
+            return $this->getProductCategory($product);
+        }
+        return null;
+    }
+
+    /**
+     * get product category for tracking purposes
+     * either based on chosen category tracking attribute
+     * or first category encountered
+     *
+     * @param $product
+     *
+     * @return mixed
+     */
+    public function getProductCategory($product)
+    {
+        $attributeCode = Mage::getStoreConfig('google/analyticsplus/categorytrackingattribute');
+        if ($attributeCode) {
+            if ($product->getResource()->getAttribute($attributeCode)) {
+                $attributeValue = $product->getAttributeText($attributeCode);
+                if (!$attributeValue) {
+                    return $product->getDataUsingMethod($attributeCode);
+                } else {
+                    return $attributeValue;
+                }
+            }
+        } else {
+            $catIds = $product->getCategoryIds();
+            foreach ($catIds as $catId) {
+                $category = Mage::getModel('catalog/category')->load($catId);
+                if ($category) {
+                    //we use the first category
+                    return $category->getName();
+                }
+            }
+        }
+        return false;
+    }
 }
